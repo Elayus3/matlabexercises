3b811e96b26965a97718eac1ef8ab6defb727b9f
Elie Youssef <33988857+Elayus3@users.noreply.github.com>
2022-03-01 14:48:48 +0100

    Update checkRows.m

--- functions/checkRows.m
+++ functions/checkRows.m
@@ -6,21 +6,26 @@ nbTrials = size(inputMatrix,1);
 
 for c = 1:nbTrials
     countHead = 0;
+    flag = 1;
     for n = 1:10
         if countHead == 0 && inputMatrix(c,n) == 2
             countHead = 1;
         elseif countHead == 5 && inputMatrix(c,n) ~= 2
             nbCorrect = nbCorrect + 1;
+            flag = 0;
             break
         elseif countHead ~= 0 && inputMatrix(c,n) ~= 2
             countHead = 0;
         elseif countHead == 5 && inputMatrix(c,n) == 2
+            flag = 0;
             break
         elseif countHead ~= 0 && inputMatrix(c,n) == 2
             countHead = countHead + 1;
         end
     end
+    if countHead == 5 && flag == 1
+        nbCorrect = nbCorrect + 1;
+    end
 end
 probability = nbCorrect/nbTrials;
 end
-
